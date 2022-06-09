4a08550fb730a391bbc7c828a9c582b433819438
tommychobo <69866834+tommychobo@users.noreply.github.com>
2022-06-08 17:04:30 -0700

    Alright, the infection now starts with 15 infected tiles, and is limited to 30 percent of the world

--- src/DinoInfection.java
+++ src/DinoInfection.java
@@ -8,12 +8,16 @@ public class DinoInfection {
     private HashSet<Point> nextInfected;
     private WorldModel world;
     private ImageStore imageStore;
+    private final int INITIAL_INFECTION = 15;
+    private final double MAX_WORLD_INFECTION = 0.3;
+    private static int infectionTotal;
 
     public DinoInfection(WorldModel world, ImageStore imageStore){
         this.world = world;
         this.imageStore = imageStore;
         infected = new boolean[world.getNumRows()][world.getNumCols()];
         nextInfected = new HashSet<>();
+        infectionTotal = 0;
     }
 
     public void motherDinoInfection(EventScheduler scheduler, Point pressed){
@@ -21,14 +25,19 @@ public class DinoInfection {
                 imageStore.getImageList("dino_mother", 32),
                 20, 15, 8, 8, this);
         infect(pressed);
+        for(int i = 1; i < INITIAL_INFECTION; i++){
+            spread();
+        }
         world.addEntity(mom);
         mom.scheduleActions(scheduler, world, imageStore);
     }
 
     public void spread(){
-        Object[] nextList = nextInfected.toArray();
-        if(nextList.length > 0)
-            infect((Point)(nextList[(int)(Math.random()*nextList.length)]));
+        if(infectionTotal*1.0/(world.getNumRows()*world.getNumCols()) < MAX_WORLD_INFECTION) {
+            Object[] nextList = nextInfected.toArray();
+            if (nextList.length > 0)
+                infect((Point) (nextList[(int) (Math.random() * nextList.length)]));
+        }
     }
 
     public boolean isInfected(Point pos){
@@ -39,6 +48,7 @@ public class DinoInfection {
     }
 
     public void infect(Point pos){
+        infectionTotal++;
         world.setBackground(pos, new Background("background_dino", imageStore.getImageList("dino_dirt")));
         infected[pos.y][pos.x] = true;
         nextInfected.remove(pos);
@@ -55,6 +65,7 @@ public class DinoInfection {
     }
 
     public void uninfect(Point pos){
+        infectionTotal--;
         world.setBackground(pos, new Background("default_background", imageStore.getImageList("grass")));
         infected[pos.y][pos.x] = false;
         List<Point> neighbors = Arrays.asList(
